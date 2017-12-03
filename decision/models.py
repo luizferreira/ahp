26688f839b55eb498c60eafcebdfdd88ac2df537
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2017-12-03 13:24:07 -0200

    Add Decision model

--- decision/models.py
+++ decision/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Decision(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    status = models.IntegerField(default=0)
+    encoded_url = models.CharField(max_length=50)
+    participants_qty = models.IntegerField()
+    creation_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title + ' (' + \
+            self.creation_date.strftime('%d/%m/%Y %H:%M') + ')'
--- decision/tests.py
+++ decision/tests.py
@@ -1,3 +1,28 @@
+from django.utils import timezone
 from django.test import TestCase
+# from test_plus.test import TestCase as PlusTestCase
 
-# Create your tests here.
+from decision.models import Decision
+
+
+class DecisionTests(TestCase):
+
+    def test_str(self):
+        decision = Decision(
+            title='Test Decision',
+            description='Test Description',
+            participants_qty=10,
+            creation_date=timezone.now(),
+        )
+        decision_str = 'Test Decision (%s)' % \
+            decision.creation_date.strftime('%d/%m/%Y %H:%M')
+        assert decision.__str__() == decision_str
+
+    def test_creation(self):
+        decision = Decision(
+            title='Test Decision',
+            description='Test Description',
+            participants_qty=10,
+            creation_date=timezone.now(),
+        )
+        assert type(decision) == Decision
